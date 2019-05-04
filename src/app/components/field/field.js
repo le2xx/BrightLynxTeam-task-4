7e20421c15643037609d9ddd4e97002e2864e728
le2xx <le2x@rambler.ru>
2019-05-04 17:14:29 +0300

    Added hSteps and vSteps date of model. Added event listener of click

--- src/app/components/field/field.js
+++ src/app/components/field/field.js
@@ -1,4 +1,33 @@
 const field = () => {
-	console.log('field');
+	const field = document.querySelector('.field');
+	const fieldItems = field.getElementsByTagName('div');
+
+	const hSteps = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']; // множество горизонтальных координат
+	const vSteps = ['1', '2', '3', '4', '5', '6', '7', '8']; // множество вертикальных координат
+
+	/*const arrayToChess = indexItems => { // конвертирует индексы массива в шахматные координаты
+		const fieldChess = Array.prototype.map.call(hSteps, (hPoint) =>
+			const h
+			hPoint + vSteps[0]);
+		console.log(fieldChess);
+	};*/
+
+	// индекс ноды в массиве нод
+	const indexNode = (node, fieldItems) => {
+		return Array.prototype.slice.call(fieldItems) // получаем индекс ноды которую нажали
+			.indexOf(node);
+	};
+
+	// обработчик события click
+	const onClickItem = node => {
+		const indexFieldItems = indexNode(node, fieldItems);
+		console.log(arrayToChess(indexFieldItems));
+	};
+
+	Array.prototype.forEach.call(fieldItems, node => // вешаем событие click на каждый элемент поля
+		node.addEventListener('click', onClickItem.bind(null, node))
+	);
+
+	//console.log(fieldItems.length);
 };
 export { field };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './styles/base.styl';
+import {field} from "./app/components/field/field";
 // import { testBlock } from './app/components/test-block/test-block';
 
 window.onload = () => {
-  // testBlock();
+  field();
 };
